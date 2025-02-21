d1bbfd59caca87993ab937bfe900d78f12e6137c
Kaiser Sakhi <mail@kaisersakhi.com>
2025-02-21 19:20:29 +0530

    Add user-agent middleware

--- scrapper/kode.py
+++ scrapper/kode.py
@@ -25,6 +25,39 @@ class KodeSpider(scrapy.Spider):
         # settings.set("CONCURRENT_REQUESTS_PER_DOMAIN", 2, priority="spider")
         settings.set("REDIRECT_MAX_TIMES", 3, priority="spider")
         settings.set("AUTOTHROTTLE_ENABLED", True, priority="spider")
+        settings.set("PROXY_POOL_ENABLED", True, priority="spider")
+        settings.set("DOWNLOADER_MIDDLEWARES", {
+            # 'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+            # 'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+            'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+            'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+        }, priority="spider")
+        settings.set("LOG_LEVEL", "WARN")
+        settings.set("USER_AGENTS", [
+            ('Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/57.0.2987.110 '
+            'Safari/537.36'),  # chrome
+            ('Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/61.0.3163.79 '
+            'Safari/537.36'),  # chrome
+            ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+            'Gecko/20100101 '
+            'Firefox/55.0'),  # firefox
+            ('Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/61.0.3163.91 '
+            'Safari/537.36'),  # chrome
+            ('Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/62.0.3202.89 '
+            'Safari/537.36'),  # chrome
+            ('Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/63.0.3239.108 '
+            'Safari/537.36'),  # chrome
+        ])
 
     def __init__(self, urls=None, *args, **kwargs):
         self.pwd = os.path.dirname(os.path.abspath(__file__))
